409069ab55481193d40d598d5b954f44c23cb47b
ernacktob <gueorgui.savadjiev@mail.mcgill.ca>
2016-01-26 19:12:36 -0500

    Added Kalmanfilter_asm func

--- Lab1/Lab1_STM32F4Cube_Base_project/RTE/Device/STM32F407VGTx/startup_stm32f407xx.s
+++ Lab1/Lab1_STM32F4Cube_Base_project/RTE/Device/STM32F407VGTx/startup_stm32f407xx.s
@@ -186,6 +186,14 @@ Reset_Handler    PROC
                  EXPORT  Reset_Handler             [WEAK]
         IMPORT  SystemInit
         IMPORT  __main
+			; Initialize FPU
+				 LDR.W	 R0, =0xE000ED88
+				 LDR	 R1, [R0]
+				 ORR	 R1, R1, #(0xF << 20)
+				 STR	 R1, [R0]
+				 DSB
+				 ISB
+			; end initialize FPU
 
                  LDR     R0, =SystemInit
                  BLX     R0
